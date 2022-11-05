1a203bc3b72401daab14d7767932714ff7b4a9d0
UrielArellano69 <115120772+UrielArellano69@users.noreply.github.com>
2022-11-04 21:47:48 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+string hora; 
+
+Console.WriteLine("Hola!, teclea la hora (hh:mm)");
+hora = Console.ReadLine();
+
+
+Console.WriteLine("La hora es: " + hora);
