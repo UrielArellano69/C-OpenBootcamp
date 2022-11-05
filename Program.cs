327ffbe32aa0bcd39b647b82727379fd88b2c0ec
UrielArellano69 <115120772+UrielArellano69@users.noreply.github.com>
2022-11-04 21:50:17 -0600

    Delete Program.cs

--- Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-string hora; 
-
-Console.WriteLine("Hola!, teclea la hora (hh:mm)");
-hora = Console.ReadLine();
-
-
-Console.WriteLine("La hora es: " + hora);
